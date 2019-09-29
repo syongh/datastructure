9e481b09ac9f0ac8c467306868d173cb2c55fded
syongh <1192687056@qq.com>
2019-09-29 11:49:57 +0800

    externtest

---
+++ week04/js/externtest/main.c
@@ -0,0 +1,7 @@
+#include<stdio.h>	
+	extern int a;
+		int main(){
+			show();
+			printf("main.c:the a is %d\n",a);
+			return 0;
+		}
---
+++ week04/js/externtest/test.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+	int a=999;
+	void show(){ 
+		printf("test.c:the a is %d\n",a);
+	}
+
