534f506ca3778e6c6c1bb790abbc9ca169878223
syongh <1192687056@qq.com>
2019-10-09 23:04:14 +0800

    array overflow

--- week04/yh/array_overflow/array_overflow.c
+++ week04/yh/array_overflow/array_overflow.c
@@ -1,18 +1,11 @@
 #include<stdio.h>
 
 int main(){
-	int k = 0;
-	int arr[3] = {0};
-	int b[3] = {0};
 	int i = 0;
-	int j = 0;
+	int arr[3] = {0};
 	printf("i = %p \n", &i);
-	printf("j = %p \n", &j);
-	printf("k = %p \n", &k);
 	printf("arr[0] = %p \n", &arr[0]);
 	printf("arr[2] = %p \n", &arr[2]);
-	printf("b[2] = %p \n", &b[0]);
-	printf("b[2] = %p \n", &b[2]);
 	
 	for(; i <= 3; i++){
 		arr[i] = 0;
